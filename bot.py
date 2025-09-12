4581541f514cf0bf173b4dedee08c2494891e5c5
Viktor Kopylov <veatekkopylov2004@gmail.com>
2025-09-12 22:45:34 +0300

    Added googletrans library to translate input text. To be woked on

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@
 # It echoes any incoming text messages.
 import asyncio
 import os
-
+from googletrans import Translator
 from telebot.async_telebot import AsyncTeleBot
 
 bot = AsyncTeleBot(os.environ["TELEGRAM_TOKEN"])
@@ -13,14 +13,16 @@ bot = AsyncTeleBot(os.environ["TELEGRAM_TOKEN"])
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
-    text = 'Hi, I am EchoBot.\nJust write me something and I will repeat it!'
+    text = 'Hi, I am Veatek_bot.\nJust write me something and I will repeat it!'
     await bot.reply_to(message, text)
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 async def echo_message(message):
-    await bot.reply_to(message, message.text)
+    translator = Translator()
+    result = translator.translate(message.text, dest = 'en')
+    await bot.reply_to(message, f'{message.text}\n\n{result.text}')
 
-
-asyncio.run(bot.polling())
+if __name__ == '__main__':
+    asyncio.run(bot.polling())
